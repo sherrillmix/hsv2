33060e9a78a35a3a2848a5cac441baba71cc8e64
sherrillmix <sherrillmix@users.noreply.github.com>
2017-03-10 12:57:32 -0500

    Move annotations to separate script

--- compareGenomes.R
+++ compareGenomes.R
@@ -1,11 +1,12 @@
-source("~/scripts/R/dna.R")
-#library(levenR)
+library(dnar)
 library(dnaplotr)
 #depends on directories:
 #  work (stores temp files)
 #  out (stores output files)
 #  ref (stores consensuses, annotations and reference sequences)
 #  dat (raw reads, dont need yet)
+
+source('readAnnotation.R')
 
 ref<-read.fa('ref/NC_001798 Reference Assembly.fasta')[1,]
 bern<-read.fa('ref/BernStein_Consensus.fasta')
@@ -21,16 +22,6 @@ names(combo)<-align$qName
 seqMat<-do.call(rbind,strsplit(combo,''))
 rownames(seqMat)<-names(combo)
 
-mismatch<-outer(1:3,1:3,function(x,y){mapply(function(xx,yy)sum(seqMat[xx,]!=seqMat[yy,]),x,y)})
-table(seqMat['ms',]==seqMat['ref',],seqMat['ms',]==seqMat['bern',])
-
-geneFa<-read.fa('ref/ncbiGenes.txt')
-annots<-read.csv('ref/Annotations_table_Hg52.csv',stringsAsFactors=FALSE)
-annots<-annots[annots$Sequence.Name!='HSVtest',]
-annots$start<-as.numeric(sub(',','',annots$Minimum))
-annots$end<-as.numeric(sub(',','',annots$Maximum))
-
-genes<-annots[annots$Type=='CDS',]
 genes$refATG<-NA
 refSeq<-paste(seqMat['ref',],collapse='')
 genes$gapStart<-noGap2Gap(refSeq,genes[,'start'])
@@ -38,18 +29,23 @@ genes$gapStartPlus2<-noGap2Gap(refSeq,genes[,'start']+2)
 genes$gapEnd<-noGap2Gap(refSeq,genes[,'end'])
 genes$gapEndMinus2<-noGap2Gap(refSeq,genes[,'end']-2)
 
-#find the codons
-genes$startCodon<-substring(refSeq,ifelse(genes$Direction=='forward',genes$gapStart,genes$gapEndMinus2),ifelse(genes$Direction=='forward',genes$gapStartPlus2,genes$gapEnd))
-genes$stopCodon<-substring(refSeq,ifelse(genes$Direction=='forward',genes$gapEndMinus2,genes$gapStart),ifelse(genes$Direction=='forward',genes$gapEnd,genes$gapStartPlus2))
 genes$msAA<-substring(paste(seqMat['ms',],collapse=''),genes$gapStart,genes$gapEnd)
 genes$msAA<-sub('X.*$','X',replaceAfterStop(dna2aa(degap(ifelse(genes$Direction=='forward',genes$msAA,revComp(genes$msAA))))))
 genes$bernAA<-substring(paste(seqMat['bern',],collapse=''),genes$gapStart,genes$gapEnd)
 genes$bernAA<-sub('X.*$','X',replaceAfterStop(dna2aa(degap(ifelse(genes$Direction=='forward',genes$bernAA,revComp(genes$bernAA))))))
 
+#find the codons
+genes$startCodon<-substring(refSeq,ifelse(genes$Direction=='forward',genes$gapStart,genes$gapEndMinus2),ifelse(genes$Direction=='forward',genes$gapStartPlus2,genes$gapEnd))
+genes$stopCodon<-substring(refSeq,ifelse(genes$Direction=='forward',genes$gapEndMinus2,genes$gapStart),ifelse(genes$Direction=='forward',genes$gapEnd,genes$gapStartPlus2))
+
 
 
 #TODO missing one ATG in reverse. is this correct?
 table(genes$startCodon,genes$Direction)
+
+
+mismatch<-outer(1:3,1:3,function(x,y){mapply(function(xx,yy)sum(seqMat[xx,]!=seqMat[yy,]),x,y)})
+table(seqMat['ms',]==seqMat['ref',],seqMat['ms',]==seqMat['bern',])
 
 genes$bernMsDiff<-apply(genes[,c('gapStart','gapEnd')],1,function(x)sum(
 	seqMat['bern',x[1]:x[2]]!=seqMat['ms',x[1]:x[2]] &
---
+++ readAnnotation.R
@@ -0,0 +1,12 @@
+library(dnar)
+
+geneFa<-read.fa('ref/ncbiGenes.txt')
+annots<-read.csv('ref/Annotations_table_Hg52.csv',stringsAsFactors=FALSE)
+annots<-annots[annots$Sequence.Name!='HSVtest',]
+annots$start<-as.numeric(sub(',','',annots$Minimum))
+annots$end<-as.numeric(sub(',','',annots$Maximum))
+
+genes<-annots[annots$Type=='CDS',]
+genes$strand<-genes$Direction=='forward'
+
+genes$row<-stackRegions(genes$start-1000,genes$end+1000)
