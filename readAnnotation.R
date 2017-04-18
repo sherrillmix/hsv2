657940f83dfb44a8e607bf6da4bf529f08ce3282
sherrillmix <sherrillmix@users.noreply.github.com>
2017-04-18 11:09:42 -0400

    Read ref seq

--- readAnnotation.R
+++ readAnnotation.R
@@ -5,9 +5,13 @@ annots<-read.csv('ref/Annotations_table_Hg52.csv',stringsAsFactors=FALSE)
 annots<-annots[annots$Sequence.Name!='HSVtest',]
 annots$start<-as.numeric(sub(',','',annots$Minimum))
 annots$end<-as.numeric(sub(',','',annots$Maximum))
+ref<-read.fa('ref/NC_001798 Reference Assembly.fasta')[1,]
 
 genes<-annots[annots$Type=='CDS',]
 genes$strand<-genes$Direction=='forward'
 
 genes$row<-stackRegions(genes$start-1000,genes$end+1000)
 genes$name<-sub(' CDS$','',genes$Name)
+seqs<-substring(ref$seq,genes$start,genes$end)
+seqs[!genes$strand]<-revComp(seqs[!genes$strand])
+genes$aa<-dna2aa(seqs)
